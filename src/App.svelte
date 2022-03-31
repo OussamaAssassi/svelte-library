2a3551cd1eddcc5d3763aebee51eeb483f3cbb93
EPHEMERA <EPHEMERA@frl-fvfxj05bhv2j.home>
2022-03-31 11:51:53 +0200

    learn svelte using library project

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Route, Router } from "svelte-routing";
+  import Create from "./library/Create.svelte"
+  import Detail from "./library/Detail.svelte";
+  import Library from "./library/Library.svelte";
+
+  export let url = "";
+</script>
+
+<style>
+  main {
+    padding: var(--spacingLarge);
+  }
+</style>
+
+<Router {url}>
+  <main>
+    <Route path="/create" let:params>
+      <Create  />
+    </Route>
+    <Route path="/books/:id" let:params>
+      <Detail id={params.id} />
+    </Route>
+    <Route path="/">
+      <Library />
+    </Route>
+  </main>
+</Router>
---
+++ src/library/Library.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from "svelte";
+    import Button from "../common/Button.svelte";
+    import BookGrid from "../library/BookGrid.svelte";
+    import { httpGet } from "../common/api.js";
+
+    let books = [];
+    onMount(async function () {
+        const { data } = await httpGet("/?_sort=id&_order=desc");
+        books = data;
+    });
+</script>
+
+<div>
+    <header>
+        <span class="preamble">Welcome to the</span>
+        <h1>Library</h1>
+    </header>
+
+    <p class="greeting">
+        This is a library for the people. Welcome. Read the books here. Be
+        inspired. Go home, and share them with your family.
+    </p>
+
+    <Button to="/create">+ Add book</Button>
+    <BookGrid {books} />
+</div>
+
+<style></style>
